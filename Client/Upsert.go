d851191718d01916585884a097f05755b5b32cfd
Pierre Vicente <pvicente@upper-link.com>
2020-09-09 16:03:15 +0200

    upsert function

--- Client/Upsert.go
+++ Client/Upsert.go
@@ -1,7 +1,55 @@
 package Client
 
-import "github.com/PierreVicente/GoXrm"
+import (
+	"bytes"
+	"github.com/PierreVicente/GoXrm"
+	"github.com/PierreVicente/GoXrm/AADAuth"
+	"github.com/google/uuid"
+	"net/http"
+)
 
-func (this *CrmServiceClient) Upsert(target GoXrm.Entity, b bool) {
+func (this *CrmServiceClient) Upsert(target GoXrm.Entity, updateOnly bool) {
+	relativeUrl := "/api/data/v" + this.ApiVersion + "/" + getCollectionSchemaName(target.LogicalName)
+	baseUrl := this.aadAuthResult.Resource
 
+	jo := GoXrm.EntityToJObject(target, "U", false)
+
+	body := jo
+	stringContent := []byte(body)
+	req, err := http.NewRequest("PATCH", baseUrl+relativeUrl, bytes.NewBuffer(stringContent))
+
+	if err != nil {
+		panic(err)
+	}
+	if this.aadAuthResult.AuthType != AADAuth.AuthType_AD {
+		req.Header.Add("Authorization", "Bearer "+this.aadAuthResult.AccessToken)
+	}
+	req.Header.Add("OData-MaxVersion", "4.0")
+	req.Header.Add("OData-Version", "4.0")
+
+	if this.CallerId != uuid.Nil {
+		req.Header.Add("MSCRMCallerID", this.CallerId.String())
+	}
+
+	req.Header.Add("Content-Type", "application/json;charset=utf-8")
+
+	if updateOnly {
+		req.Header.Add("If-Match", "*")
+	}
+
+	client := &http.Client{}
+	var resp, err2 = client.Do(req)
+	if err2 != nil {
+		panic(err2)
+	}
+	defer resp.Body.Close()
+
+	r1 := ""
+
+	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+		r1 = resp.Header.Get("OData-EntityId")
+		r1 = r1[1:37]
+	} else {
+		panic("marchepas")
+	}
 }
